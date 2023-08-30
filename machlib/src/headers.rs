08d09fe033305cc02d0f36ebf5d2e8ccc15aaed8
HBchevelle68 <aportante10@gmail.com>
2023-08-29 21:56:11 -0400

    storing

--- machlib/src/headers.rs
+++ machlib/src/headers.rs
@@ -1,3 +1,4 @@
+use plain::Plain;
 use scroll::{Pread, Pwrite, SizeWith};
 
 /// The 64-bit mach header magic
@@ -10,8 +11,10 @@ pub const MH_CIGAM_64: u32 = 0xcffaedfe;
 pub const MH_MAGIC: u32 = 0xfeedface;
 pub const MH_CIGAM: u32 = 0xcefaedfe;
 
+pub const MH_64_SIZE: usize = 32;
+
 #[repr(C)]
-#[derive(Clone, Copy, Default, Debug, Pread, Pwrite, SizeWith)]
+#[derive(Clone, Copy, Default, Pread, Pwrite, SizeWith)]
 /// 64-bit Mach-o header
 pub struct MachHeader64 {
     /// mach magic number identifier
@@ -31,4 +34,25 @@ pub struct MachHeader64 {
     pub reserved: u32,
 }
 
-impl MachHeader64 {}
+unsafe impl Plain for MachHeader64 {}
+
+impl MachHeader64 {
+    pub fn from_bytes(bytes: &[u8; MH_64_SIZE]) -> &Self {
+        plain::from_bytes(bytes).unwrap()
+    }
+}
+
+impl core::fmt::Debug for MachHeader64 {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.debug_struct("Header")
+            .field("magic", &format_args!("0x{:x}", self.magic))
+            .field("cputype", &format_args!("0x{:x}", self.cputype))
+            .field("cpusubtype", &format_args!("0x{:x}", self.cpusubtype))
+            .field("filetype", &format_args!("0x{:x}", self.filetype))
+            .field("ncmds", &self.ncmds)
+            .field("sizeofcmds", &self.sizeofcmds)
+            .field("flags", &format_args!("0x{:x}", self.flags))
+            .field("reserved", &format_args!("0x{:x}", self.reserved))
+            .finish()
+    }
+}
